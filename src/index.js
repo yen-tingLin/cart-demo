c11acbde4582107e7b99083f9da38ff303fad483
yen-tingLin <aillvone@gmail.com>
2021-03-17 11:48:40 +0800

    setup image upload

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
-require("../config/mongoose.js")(app);
+require("./config/mongoose.js")(app);
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('./files', express.static('files'));
 
 app.get('/', (req, res) => {
     res.json({
