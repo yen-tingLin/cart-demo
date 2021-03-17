1dd8cd31553854f35bfed5c4c675e30e4ca5c687
yen-tingLin <aillvone@gmail.com>
2021-03-17 11:23:43 +0800

    setup repository

--- src/app/repository.js
+++ src/app/repository.js
@@ -1,1 +1,18 @@
+const Product = require("./model");
 
+exports.retrieveProducts = async () => {
+    const products = await Product.find();
+    return products;
+};
+exports.retrieveProductById = async id => {
+    const product = await Product.findById(id);
+    return product;
+};
+exports.createProduct = async payload => {
+    const newProduct = await Product.create(payload);
+    return newProduct;
+};
+exports.removeProduct = async id => {
+    const product = await Product.findById(id);
+    return product;
+}	
